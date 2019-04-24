63cb571be72ddf0ce0e81c0afdbe3f795da63c24
Karol Bulwin <karolbulwin@gmail.com>
2019-04-24 22:14:34 +0200

    extract components

---
+++ src/AppCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { CardList } from "./CardList";
+import { Form } from "./components/Form";
+export class AppCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.title = "The GitHub Cards App";
+  }
+  state = {
+    profiles: []
+  };
+  addNewProfile = profileData => {
+    console.log(profileData);
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  };
+  render() {
+    return (
+      <div className="appCard">
+        <div className="header">{this.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
